796cc484f4b12e21e1fc239ed9b7b8e1023ebc97
Owen Zhao <owenzz.hi@gmail.com>
2026-08-07 11:57:44 -0700

    fixed errors

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 with st.form("calculator"):
     num1 = st.number_input("First number")
     num2 = st.number_input("Second number")
-    submitted = st.form_submit_button("add+ballons", disabled = st.session_state.busy)
+    submitted = st.form_submit_button("add+ballons")
     if (submitted):
         st.write(num1+num2)
         st.balloons()
