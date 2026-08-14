66ba91d35a759c85f8404b9dfb8d8bb7e582ad90
Owen Zhao <owenzz.hi@gmail.com>
2026-08-14 13:21:41 -0700

    file_names visible now

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,14 +50,14 @@ groq_client = getGroq()
 
 top_left_col,main_header_col = st.columns([1,15],vertical_alignment = "center")
 collection = chroma_client.get_or_create_collection(f"collection{st.session_state.current_chat_id}")
+curr = st.session_state.chats[st.session_state.current_chat_id]   
 
 def deleteButton(indexToDelete):
     st.session_state.chats.pop(indexToDelete)
 with main_header_col:
     st.title("CHATBOT!")
 if st.button("🗑️ Clear Chat File History"):
-    with st.chat_message("system",avatar="🖥️"):
-        st.write("Files received!")
+    curr["msg"].append({"role":"system", "avatar":"🖥️","content":"File History Cleared!"})
     chroma_client.delete_collection(f"collection{st.session_state.current_chat_id}")
     st.rerun()
 with top_left_col:
@@ -100,7 +100,6 @@ if (st.session_state.easter_eggs):
     emoji_float(emojis=["🔥", "🚀", "🎉","😄","🤩","🥳"],count = 50,minSize = 50,maxSize = 100,animationLength = 3,key = len(st.session_state.chats[st.session_state.current_chat_id]["docs"]))
 model_call = {"Meta Llama 3.1":"llama-3.1-8b-instant","OpenAI OSS 20B":"openai/gpt-oss-20b","OpenAI OSS 120B":"openai/gpt-oss-120b"}
 MODEL = model_call[st.session_state.model]
-curr = st.session_state.chats[st.session_state.current_chat_id]   
 for message in curr["msg"]:
     with st.chat_message(message["role"],avatar = message.get("avatar")):
         st.write(message["content"])
@@ -136,8 +135,8 @@ if not curr["busy"]:
                 tags = [file.name + str(i) for i in range(len(chunks))]
                 collection.add(documents=chunks,ids=tags)
                 with st.chat_message("system",avatar="🖥️"):
-                    st.write("Files received!")
-                curr["msg"].append({"role":"system", "avatar":"🖥️","content":"Files received!"})
+                    st.write("File " + file.name+ " received!")
+                curr["msg"].append({"role":"system", "avatar":"🖥️","content":"File " + file.name+ " received!"})
             if (question_text.strip()):
                 curr["msg"].append({"role":"user","content":question_text})
                 curr["docs"].append({"role":"user","content":question_text})
